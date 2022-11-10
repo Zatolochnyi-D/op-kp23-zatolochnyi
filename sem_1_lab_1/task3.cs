dd02b98c85da21cfb632ecf390595c8ac5c8702d
Denis <d.zatolochnyi@gmail.com>
2022-11-10 13:54:09 +0200

    Added test cases for task3

--- sem_1_lab_1/task3.cs
+++ sem_1_lab_1/task3.cs
@@ -6,10 +6,24 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
+            //test input:
+            //case 1: n = 3, x = 2
+            //case 2: n = 5, x = 4
+            //case 3: n = 9, x = 3
+            //case 4: n = 12, x = 13
+            //case 5: n = 17, x = 0.5
+
             double x; //input
             int n; //input
             double power; //output
             int factorial; //output
+
+            //test output:
+            //case 1: factorial = 6, power = 8
+            //case 2: factorial = , power = 1024
+            //case 3: factorial = , power = 19683
+            //case 4: factorial = , power = 23298085
+            //case 5: factorial = , power ≈ 0.000007629
         }
     }
 }
