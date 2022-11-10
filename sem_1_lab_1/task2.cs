7e5f58246c0da709c430dd2b987a2722306f6629
Denis <d.zatolochnyi@gmail.com>
2022-11-10 10:02:05 +0200

    Added test cases for task2

--- sem_1_lab_1/task2.cs
+++ sem_1_lab_1/task2.cs
@@ -6,8 +6,25 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
+            //test input:
+            //case 1: n = 7
+            //case 2: n = 13
+            //case 3: n = 26
+            //case 4: n = 59
+            //case 5: n = 153
+            //case 6: n = 187
+            //case 7: n = 19467
+
             int n; //input
 
+            //test output:
+            //case 1: True
+            //case 2: True
+            //case 3: False
+            //case 4: True
+            //case 5: False
+            //case 6: True
+            //case 7: False
         }
     }
 }
