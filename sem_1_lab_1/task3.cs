28549a8fe28918d859ee525045b5173447d9127f
Denis <d.zatolochnyi@gmail.com>
2022-11-10 11:19:32 +0200

    Created prototype of task3

---
+++ sem_1_lab_1/task3.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Assignment1
+{
+    class Task3
+    {
+        static void Main(string[] args)
+        {
+            double x, n; //input
+            double factorial, power; //output
+        }
+    }
+}
