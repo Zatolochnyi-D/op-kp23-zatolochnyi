b7a86dcbbb3c22f93d5e42b6f78763ef4f9d8bda
Denis <d.zatolochnyi@gmail.com>
2022-11-12 13:56:05 +0200

    Added test cases to task1

--- sem_1_lab_1/task1.cs
+++ sem_1_lab_1/task1.cs
@@ -6,8 +6,33 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
+            //test input:
+            //case 1: x0 = 0; xn = 8; n = 10
+            //case 2: x0 = 3; xn = -6, n = 7
+
             double x0, xn, n; //input
             double x, yx; //output
+
+            //test output:
+            //case 1:
+            //  x1 = 0.8, y(x) ≈ 1.755
+            //  x2 = 1.6, y(x) ≈ 1.991
+            //  x3 = 2.4, y(x) ≈ 2.241
+            //  x4 = 3.2, y(x) ≈ 2.527
+            //  x5 = 4, y(x) ≈ 2.872
+            //  x6 = 4.8, y(x) ≈ 3.295
+            //  x7 = 5.6, y(x) ≈ 3.815
+            //  x8 = 6.4, y(x) ≈ 4.452
+            //  x9 = 7.2, y(x) ≈ 5.224
+            //  x10 = 8, y(x) ≈ 6.149
+            //case 2:
+            //  x1 ≈ 1.714, y(x) ≈ 2.025
+            //  x2 ≈ 0.429, y(x) ≈ 1.643
+            //  x3 ≈ -0.857, y(x) ≈ 1.185
+            //  x4 ≈ -2.143, y(x) ≈ 0.301
+            //  x5 ≈ -3.429, y(x) does not exist
+            //  x6 ≈ -4.714, y(x) does not exist
+            //  x7 = -6, y(x) does not exist
         }
     }
 }
