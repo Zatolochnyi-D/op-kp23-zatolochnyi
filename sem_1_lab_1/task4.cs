e244a503050754a79461f78a48e6634b5d81c5e0
Denis <d.zatolochnyi@gmail.com>
2022-11-12 20:12:35 +0200

    Added test cases to task4

--- sem_1_lab_1/task4.cs
+++ sem_1_lab_1/task4.cs
@@ -6,8 +6,32 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
+            //test input:
+            //case 1: x = 3
+            //case 2: x = 5
+            //case 3: x = 9
+            //case 4: x = 13
+            //case 5: x = 35
+            //case 6: x = 457
+            //case 7: x = 5358
+            //case 8: x = -4
+            //case 9: x = -234
+            //case 10: x = -2355
+
             double x; //input
             double sin; //output
+
+            //test output:
+            //case 1: sin(x) ≈ 0.1411
+            //case 2: sin(x) ≈ -0.9589
+            //case 3: sin(x) ≈ 0.4121
+            //case 4: sin(x) ≈ 0.4202
+            //case 5: sin(x) ≈ -0.4282
+            //case 6: sin(x) ≈ -0.9948
+            //case 7: sin(x) ≈ -0.9999
+            //case 8: sin(x) ≈ 0.7568
+            //case 9: sin(x) ≈ -0.9988
+            //case 10: sin(x) ≈ 0.9300
         }
     }
 }
