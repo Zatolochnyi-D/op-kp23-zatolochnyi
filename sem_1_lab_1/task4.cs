f2a93655988878a9c3fd26040073c3b8c98b82d1
Denis <d.zatolochnyi@gmail.com>
2022-11-12 20:03:15 +0200

    Created prototype of task4

---
+++ sem_1_lab_1/task4.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Assignment1
+{
+    class Task4
+    {
+        static void Main(string[] args)
+        {
+            double x; //input
+            double sin; //output
+        }
+    }
+}
