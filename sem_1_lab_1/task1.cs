8ba16cc9550ebce59a57ea31834405db4b54065b
Denis <d.zatolochnyi@gmail.com>
2022-11-12 11:48:57 +0200

    Created prototype of task1

---
+++ sem_1_lab_1/task1.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Assignment1
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            double x0, xn, n; //input
+            double x, yx; //output
+        }
+    }
+}
