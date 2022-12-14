3582913607db7357d76e2bee60fb8144aa0594af
Denis <d.zatolochnyi@gmail.com>
2022-12-14 19:50:06 +0200

    Created prototype of the program

---
+++ sem_1_lab_2/program.cs
@@ -0,0 +1,99 @@
+using System;
+
+namespace Assignment2
+{
+    class Percolate
+    {
+        //size of side
+        static int n;
+        //amount of opened sites
+        static int opened = 0;
+        //matrix to demonstrate results
+        static int[,] matrix;
+        //array to do calculations
+        static int[] arr;
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+        //create 2D matrix nxn with blocked sites, create 1D array n*n with indexs as elements
+        static void Init()
+        {
+            
+        }
+
+        //open choosed site
+        static void Open(int row, int col)
+        {
+            
+        }
+
+        //return true if choosed site is opened
+        static bool IsOpen(int row, int col)
+        {
+            
+        }
+
+        //return true if choosed site is full
+        static bool IsFull(int row, int col)
+        {
+            
+        }
+
+        //return number of opened sites
+        static int NumberOfOpenSites()
+        {
+            
+        }
+
+        //print matrix
+        static void PrintMatrix()
+        {
+            
+        }
+
+        //print array
+        static void PrintArray()
+        {
+            
+        }
+
+        //print array like a matrix
+        static void PrintArrayM()
+        {
+            
+        }
+
+        //connect 2 points
+        static void union(int x, int y)
+        {
+            
+        }
+
+        //convert matrix coordinates into array coordinates
+        static int Convert2in1(int i, int j)
+        {
+            
+        }
+
+        //connect points
+        static void Connect()
+        {
+            
+        }
+
+        //replace all 1 with 2 where it is necessary and print true if this matrix perlocates
+        static bool Flow()
+        {
+            
+        }
+
+        //test of all functions
+        static void Test()
+        {
+            
+        }
+    }
+}
