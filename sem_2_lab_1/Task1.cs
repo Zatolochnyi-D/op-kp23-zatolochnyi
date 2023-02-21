d58a8a500b1d7d9993cf6258361e56eb9488ae58
denis <d.zatolochnyi@gmail.com>
2023-02-21 16:01:13 +0200

    Added prototype of Task 1

---
+++ sem_2_lab_1/Task1.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+
+namespace Assignment1
+{
+    public class Program
+    {
+        static string pathToFile = "../../../";
+
+        static void Main()
+        {
+            
+        }
+        
+        static void Write(string path)
+        {
+            
+        }
+        
+        static void Read(string path)
+        {
+            
+        }
+    }
+}
