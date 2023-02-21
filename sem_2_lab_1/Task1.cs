1edef15a20e3a36b9b0e0b7e81ea6cc41146f423
denis <d.zatolochnyi@gmail.com>
2023-02-21 16:54:20 +0200

    Implementation of Task 1

--- sem_2_lab_1/Task1.cs
+++ sem_2_lab_1/Task1.cs
@@ -3,23 +3,41 @@ using System.IO;
 
 namespace Assignment1
 {
-    public class Program
+    public class Task1
     {
         static string pathToFile = "../../../";
 
         static void Main()
         {
-            
+            Task1Test();
         }
         
         static void Write(string path, params string[] text)
         {
-            
+            using (StreamWriter sw = new(path, false))
+            {
+                foreach (string s in text)
+                {
+                    sw.WriteLine(s);
+                }
+            }
         }
         
         static void Read(string path)
         {
-            
+            using (StreamReader sr = new(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    Console.WriteLine(sr.ReadLine());
+                }
+            }
+        }
+
+        static void Task1Test()
+        {
+            Write(pathToFile + "Task1.txt", "Hello", "world");
+            Read(pathToFile + "Task1.txt");
         }
     }
 }
