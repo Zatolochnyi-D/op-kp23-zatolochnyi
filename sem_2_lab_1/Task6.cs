9a4792c4b62dbdf815b146ca9026d858d518bc13
denis <d.zatolochnyi@gmail.com>
2023-02-27 12:14:56 +0200

    Created prototype of Task 6

---
+++ sem_2_lab_1/Task6.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+
+namespace Assignment1
+{
+    public class Task6
+    {
+        static string pathToFile = "../../../";
+
+        static void Main()
+        {
+            Task6Test();
+        }
+
+        static void ToBinary(string path)
+        {
+
+        }
+
+        static void RewriteBinary(string fromPath, string toPath)
+        {
+
+        }
+
+        static void ReadBinary(string path)
+        {
+
+        }
+
+        static void Task6Test()
+        {
+
+        }
+    }
+}
