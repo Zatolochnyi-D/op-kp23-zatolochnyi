f987d35df66075791aaf89b61dfb9d387866997b
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2023-03-31 17:11:57 +0300

    Created Prototype of Task1.1, added test case

---
+++ sem_2_lab_2/Task1.1.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace Assignment2
+{
+   class Task1
+   {
+       static void Main()
+       {
+           
+       }
+   }
+
+   public class Employee
+   {
+       public Employee(string lastName, int salary, int withheld)
+       {
+           
+       }
+
+       private void CalcIssued()
+       {
+           
+       }
+
+       public string GetInfo()
+       {
+           return "";
+       }
+   }
+}
+
+//input:
+//Emp#0001 3000 1000
+//Emp#0002 5000 1500
+//Emp#0003 2000 500
+//Emp#0004 3000 1000
+
+//expected output:
+//Last Name   Salary   Withheld   Issued
+//Emp#0001    3000     1000       2000
+//Emp#0002    5000     1500       3500
+//Emp#0003    2000     500        1500
+//Emp#0004    3000     1000       2000
