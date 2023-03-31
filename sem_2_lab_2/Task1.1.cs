dfa3acc5706b2ed3687bec406ebf18022a4599c6
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2023-03-31 17:51:03 +0300

    Implementation of Task1.1

--- sem_2_lab_2/Task1.1.cs
+++ sem_2_lab_2/Task1.1.cs
@@ -2,31 +2,80 @@ using System;
 
 namespace Assignment2
 {
-   class Task1
-   {
-       static void Main()
-       {
-           
-       }
-   }
+    class Task1
+    {
 
-   public class Employee
-   {
-       public Employee(string lastName, int salary, int withheld)
-       {
-           
-       }
+        static string[] lastNames = new string[]
+        {
+            "Emp#0001",
+            "Emp#0002",
+            "Emp#0003",
+            "Emp#0004",
+        };
 
-       private void CalcIssued()
-       {
-           
-       }
+        static int[] salaries = new int[]
+        {
+            3000,
+            5000,
+            2000,
+            3000,
+        };
 
-       public string GetInfo()
-       {
-           return "";
-       }
-   }
+        static int[] withhelds = new int[]
+        {
+            1000,
+            1500,
+            500,
+            1000,
+        };
+
+        static void Main()
+        {
+            Employee[] employees = new Employee[lastNames.Length];
+            for (int i = 0; i < lastNames.Length; i++)
+            {
+                employees[i] = new(lastNames[i], salaries[i], withhelds[i]);
+            }
+
+            Console.WriteLine("Last Name\tSalary\tWithheld\tIssued");
+            for (int i = 0; i < employees.Length; i++)
+            {
+                Console.WriteLine(employees[i].GetInfo());
+            }
+        }
+    }
+
+    public class Employee
+    {
+        private string _lastName;
+        private int _salary;
+        private int _withheld;
+        private int _issued = 0;
+
+        public string LastName { get => _lastName; }
+        public int Salary { get => _salary; }
+        public int Withheld { get => _withheld; }
+        public int Issued { get => _issued; }
+
+        public Employee(string lastName, int salary, int withheld)
+        {
+            _lastName = lastName;
+            _salary = salary;
+            _withheld = withheld;
+
+            CalcIssued();
+        }
+
+        private void CalcIssued()
+        {
+            _issued = _salary - _withheld;
+        }
+
+        public string GetInfo()
+        {
+            return $"{_lastName}\t{_salary}\t{_withheld}\t\t{_issued}";
+        }
+    }
 }
 
 //input:
@@ -41,3 +90,5 @@ namespace Assignment2
 //Emp#0002    5000     1500       3500
 //Emp#0003    2000     500        1500
 //Emp#0004    3000     1000       2000
+
+//result is equal to expected output
