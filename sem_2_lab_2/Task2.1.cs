fd1ee0a796bfbac67ee41c42291328d5a67ec572
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2023-04-04 18:57:08 +0300

    Implementation of Task2.1

--- sem_2_lab_2/Task2.1.cs
+++ sem_2_lab_2/Task2.1.cs
@@ -6,7 +6,38 @@ namespace Assignment2
 	{
 		static void Main()
 		{
-            
+            Vessel[] vessels = new Vessel[5];
+            Random rnd = new();
+
+            for (int i = 0; i < vessels.Length; i++)
+            {
+                switch (rnd.Next(0, 2))
+                {
+                    case 0:
+                        vessels[i] = new SailingVessel();
+                        Console.WriteLine("Ship created");
+                        break;
+
+                    case 1:
+                        vessels[i] = new Submarine();
+                        Console.WriteLine("Submarine created");
+                        break;
+                }
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < vessels.Length; i++)
+            {
+                vessels[i].PrepareToMove();
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < vessels.Length; i++)
+            {
+                vessels[i].Move();
+            }
         }
 	}
 
@@ -26,12 +57,12 @@ namespace Assignment2
 
         public override void Move()
         {
-            
+            Console.WriteLine("Ship is moving");
         }
 
         public override void PrepareToMove()
         {
-            
+            Console.WriteLine("Ship raises it's sails");
         }
     }
 
@@ -44,12 +75,12 @@ namespace Assignment2
 
         public override void Move()
         {
-            
+            Console.WriteLine("Submarine is moving");
         }
 
         public override void PrepareToMove()
         {
-            
+            Console.WriteLine("Submarine starts it's engines");
         }
     }
 }
