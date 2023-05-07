1adad881e0c1b679aa54b46bd2db08b4726d0c94
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2023-05-07 14:43:35 +0300

    Created assignment 3
    Created folder, added readme file and created prototype of the program

---
+++ sem_2_lab_3/Deque.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Assignment
+{
+	// array-based deque
+	public class ADeque
+	{
+		public ADeque()
+		{
+
+		}
+	}
+
+	// linked list-based deque
+	public class LDeque
+	{
+		public LDeque()
+		{
+
+		}
+	}
+}
