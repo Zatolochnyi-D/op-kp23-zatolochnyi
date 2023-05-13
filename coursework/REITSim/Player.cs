f5d25ab18798ecea4c41f057677ee22e63a9d03b
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2023-05-13 19:40:27 +0300

    Coursework first step
    Created coursework's folder, added readme file with small description and prototype of the program

---
+++ coursework/REITSim/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GameMechanics
+{
+	public class Player
+	{
+		protected string _name;
+		protected int _reputation;
+		protected int _money;
+
+		public Player()
+		{
+
+		}
+	}
+}
+
