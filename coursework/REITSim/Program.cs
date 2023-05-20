a1adc15b28faf7894055325a69ec3caa3bf54410
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2023-05-20 12:35:32 +0300

    Shares part
    Class Investor removed, used Shares instead. Class Shares reworked. Created ability to sell-buy shares. Created test for this ability.

--- coursework/REITSim/Program.cs
+++ coursework/REITSim/Program.cs
@@ -6,6 +6,43 @@ namespace GameMechanics
     {
         static void Main()
         {
+            SharesTradingTest(21.2);
+        }
+
+        static void PlayerStats(Player player)
+        {
+            Console.WriteLine("Player stats:");
+            Console.WriteLine($"Player shares: {player.PlayerShares.Percent}");
+            Console.WriteLine($"Shares on exchange: {player.SharesOnExchange.Percent}");
+            Console.WriteLine($"Player reputation: {player.Reputation}");
+            Console.WriteLine($"Player money: {player.Money}");
+            Console.WriteLine($"Shares price: {player.SharePrice}");
+            Console.WriteLine($"Investor shares: {player.InvestorShares}");
+            Console.WriteLine();
+        }
+
+        static void SharesTradingTest(double percent)
+        {
+            Player player = new();
+
+            PlayerStats(player);
+
+            Console.WriteLine($"Sell {percent}% of shares:");
+            player.SharesToSell(percent);
+
+            PlayerStats(player);
+
+            Console.WriteLine("Next turn while player have shares to sell:");
+            while (player.SharesOnExchange.Percent != 0.0)
+            {
+                player.NextTurn();
+                PlayerStats(player);
+            }
+
+            Console.WriteLine("Buy shares back:");
+            player.BuyShares(percent);
+
+            PlayerStats(player);
         }
     }
 }
