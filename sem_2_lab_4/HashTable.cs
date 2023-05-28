d4a80bd8b76b9329e3c4883219af33e577e3b70f
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2023-05-28 18:23:36 +0300

    HashTable prototype
    Interface of Hash Table

---
+++ sem_2_lab_4/HashTable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Assignment
+{
+	interface IHashTable<Ktype, Vtype>
+	{
+		int Count { get; }
+
+		Ktype[] Keys { get; }
+
+		void Add(Ktype key, Vtype value);
+
+		void Remove(Ktype key);
+
+		Vtype Get(Ktype key);
+
+		bool ContainsKey(Ktype key);
+
+		bool ContainsValue(Vtype value);
+
+		void Clear();
+	}
+}
