64c863ba5231c38ab62b862e3d5fc14ee7fad339
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2023-05-30 20:42:48 +0300

    GUI prototype

--- coursework/REITSim/main.py
+++ coursework/REITSim/main.py
@@ -1,17 +1,19 @@
 from tkinter import *
+from SceneManager import SceneManager
+from Scenes import *
 import os
 import clr
 
-pathToDll = os.getcwd() + "/bin/Debug/net7.0/REITSim.dll"
-clr.AddReference(pathToDll)
-
-import GameMechanics as gm
+# pathToDll = os.getcwd() + "/bin/Debug/net7.0/REITSim.dll"
+# clr.AddReference(pathToDll)
+#
+# import GameMechanics as gm
 
 tk = Tk()
-player = gm.Player()
+tk.geometry(f"{global_settings['width']}x{global_settings['height']}")
+tk.resizable(global_settings["width_resize"], global_settings["height_resize"])
 
-btn = Button(text="PlayerMoney", command=lambda: print(player.Money))
-
-btn.pack()
+sc = SceneManager(tk, ["main menu"])
+sc.load_scenes(pages)
 
 tk.mainloop()
