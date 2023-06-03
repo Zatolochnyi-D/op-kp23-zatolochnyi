85f072fc473f4f2dff0374854ca058738ef26ef9
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2023-06-03 18:23:41 +0300

    New SLList
    Added interface for linked lists. Added new structure - Sorted Single-Linked List.

--- coursework/REITSim/SLList.cs
+++ coursework/REITSim/SLList.cs
@@ -3,7 +3,26 @@ using System.Collections;
 
 namespace CustomCollections
 {
-    // single-linked node
+    interface ILinkedList<T> : ICloneable, IEnumerable<T>
+    {
+        int Count { get; }
+
+        void Add(T item);
+
+        void Remove(T item);
+
+        void RemoveAt(int index);
+
+        int IndexOf(T item);
+
+        bool Contains(T item);
+
+        void Clear();
+
+        void CopyTo(T[] array, int arrayIndex);
+    }
+
+
     public class SLNode<T>
     {
         public T data;
@@ -17,22 +36,19 @@ namespace CustomCollections
     }
 
 
-    // single-linked list enumerator
-    public class SLListEnumerator<T> : IEnumerator<T>
+    public class SLListEnumerator<T>: IEnumerator<T>
     {
-        private SLNode<T>? current;
+        private SLNode<T> current;
 
-        // O(1)
-        public SLListEnumerator(SLList<T> list)
+        public SLListEnumerator(SLNode<T>? head)
         {
-            current = new(default, list.Head);
+            current = new(default, head);
         }
 
         public T Current => current.data;
 
         object IEnumerator.Current => Current;
 
-        // O(1)
         public bool MoveNext()
         {
             if (current.next != null)
@@ -49,23 +65,16 @@ namespace CustomCollections
     }
 
 
-    // single-linked list
-    public class SLList<T> : IList<T>, ICloneable
+    public class SLList<T> : ILinkedList<T>
     {
-        private SLNode<T>? _head;
-        private SLNode<T>? _tale;
-        private int _size;
-
-        internal SLNode<T>? Head => _head;
+        protected SLNode<T>? _head;
+        protected SLNode<T>? _tale;
+        protected int _size;
 
         public int Count => _size;
 
-        public bool IsReadOnly => false;
-
-        // O(1) / O(n)
         public T this[int index] { get => GetAt(index); set => SetAt(index, value); }
 
-        // O(1)
         public SLList()
         {
             _head = null;
@@ -73,7 +82,6 @@ namespace CustomCollections
             _size = 0;
         }
 
-        // O(n)
         public SLList(params T[] values)
         {
             _head = null;
@@ -86,8 +94,19 @@ namespace CustomCollections
             }
         }
 
-        // O(1) / O(n)
-        private T GetAt(int index)
+        public SLList(SLList<T> list)
+        {
+            _head = null;
+            _tale = null;
+            _size = 0;
+
+            foreach (T el in list)
+            {
+                Add(el);
+            }
+        }
+
+        protected T GetAt(int index)
         {
             if (index < 0 || index >= _size)
             {
@@ -113,8 +132,7 @@ namespace CustomCollections
             }
         }
 
-        // O(1) / O(n)
-        private void SetAt(int index, T item)
+        protected void SetAt(int index, T item)
         {
             if (index < 0 || index > _size)
             {
@@ -144,7 +162,6 @@ namespace CustomCollections
             }
         }
 
-        // O(1)
         public void Add(T item)
         {
             switch (_tale)
@@ -163,7 +180,6 @@ namespace CustomCollections
             }
         }
 
-        // O(1) / O(n)
         public void Insert(int index, T item)
         {
             if (index < 0 || index > _size)
@@ -192,7 +208,6 @@ namespace CustomCollections
             }
         }
 
-        // O(n)
         public int IndexOf(T item)
         {
             if (_head == null)
@@ -213,22 +228,19 @@ namespace CustomCollections
             return -1;
         }
 
-        // O(n)
-        public bool Remove(T item)
+        public void Remove(T item)
         {
             int i = IndexOf(item);
             if (i == -1)
             {
-                return false;
+                return;
             }
             else
             {
                 RemoveAt(i);
-                return true;
             }
         }
 
-        // O(1) / O(n)
         public void RemoveAt(int index)
         {
             if (index < 0 || index >= _size)
@@ -254,7 +266,6 @@ namespace CustomCollections
             }
         }
 
-        // O(1)
         public void Clear()
         {
             _head = null;
@@ -262,13 +273,11 @@ namespace CustomCollections
             _size = 0;
         }
 
-        // O(n)
         public bool Contains(T item)
         {
             return IndexOf(item) == -1 ? false : true;
         }
 
-        // O(n)
         public void CopyTo(T[] array, int arrayIndex)
         {
             if (_size == 0 || _size < arrayIndex)
@@ -293,31 +302,258 @@ namespace CustomCollections
             }
         }
 
-        // O(n)
         public object Clone()
         {
             T[] array = new T[Count];
             CopyTo(array, 0);
 
             return new SLList<T>(array);
-
-            // O(n^2)
-            //for (int i = 0; i < Count; i++)
-            //{
-            //    list[i] = this[i];
-            //}
         }
 
-        // O(1)
         public IEnumerator<T> GetEnumerator()
         {
-            return new SLListEnumerator<T>(this);
+            return new SLListEnumerator<T>(_head);
         }
 
-        // O(1)
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+
+    public class SortedSLList<T> : ILinkedList<T>
+    {
+        protected SLNode<T>? _head;
+        protected SLNode<T>? _tale;
+        protected int _size;
+
+        // if true, put item on this place, else go ahead.
+        protected Func<T, T, bool> _compareFunc;
+
+        public T this[int index] { get => GetAt(index); }
+
+        public int Count => _size;
+
+        public SortedSLList(Func<T, T, bool> func)
+        {
+            _head = null;
+            _tale = null;
+            _size = 0;
+
+            _compareFunc = func;
+        }
+
+        public SortedSLList(Func<T, T, bool> func, params T[] values)
+        {
+            _head = null;
+            _tale = null;
+            _size = 0;
+
+            _compareFunc = func;
+
+            foreach (T el in values)
+            {
+                Add(el);
+            }
+        }
+
+        protected T GetAt(int index)
+        {
+            if (index < 0 || index >= _size)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else if (index == 0)
+            {
+                return _head.data;
+            }
+            else if (index == _size - 1)
+            {
+                return _tale.data;
+            }
+            else
+            {
+                SLNode<T> node = _head;
+                for (int i = 0; i < index; i++)
+                {
+                    node = node.next;
+                }
+
+                return node.data;
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (_head == null) // _size == 0
+            {
+                _head = new(item, null);
+                _tale = _head;
+                _size++;
+            }
+            else if (_head.next == null) // _size == 1
+            {
+                if (_compareFunc(_head.data, item))
+                {
+                    _head = new(item, _head);
+                    _tale = _head.next;
+                }
+                else
+                {
+                    _head.next = new(item, null);
+                    _tale = _head.next;
+                }
+                _size++;
+            }
+            else
+            {
+                SLNode<T> prev = _head;
+                SLNode<T> cur = _head.next;
+
+                if (_compareFunc(prev.data, item))
+                {
+                    _head = new(item, _head);
+                }
+                else
+                {
+                    while (true)
+                    {
+                        if (_compareFunc(cur.data, item))
+                        {
+                            prev.next = new(item, cur);
+                            return;
+                        }
+
+                        if (cur.next == null)
+                        {
+                            break;
+                        }
+
+                        prev = cur;
+                        cur = cur.next;
+                    }
+
+                    _tale.next = new(item, null);
+
+                    _tale = _tale.next;
+                }
+
+                _size++;
+            }
+        }
+
+        public void Remove(T item)
+        {
+            int i = IndexOf(item);
+            if (i == -1)
+            {
+                return;
+            }
+            else
+            {
+                RemoveAt(i);
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= _size)
+            {
+                throw new ArgumentOutOfRangeException("RemoveAt", "Index out of range");
+            }
+            else if (index == 0)
+            {
+                _head = _head.next;
+                _size--;
+            }
+            else
+            {
+                SLNode<T> node = _head;
+                for (int i = 0; i < index - 1; i++)
+                {
+                    node = node.next;
+                }
+
+                node.next = node.next.next;
+                _tale = node;
+                _size--;
+            }
+        }
+
+        public int IndexOf(T item)
+        {
+            if (_head == null)
+            {
+                return -1;
+            }
+
+            SLNode<T> node = _head;
+            for (int i = 0; i < _size; i++)
+            {
+                if (node.data.Equals(item))
+                {
+                    return i;
+                }
+                else node = node.next;
+            }
+
+            return -1;
+        }
+
+        public bool Contains(T item)
+        {
+            return IndexOf(item) != -1;
+        }
+
+        public void Clear()
+        {
+            _head = null;
+            _tale = null;
+            _size = 0;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (_size == 0 || _size < arrayIndex)
+            {
+                throw new ArgumentException("ArrayIndex bigger than size of the list");
+            }
+            else if (_size - arrayIndex > array.Length)
+            {
+                throw new ArgumentException("Destination array is too small");
+            }
+
+            SLNode<T> node = _head;
+            for (int i = 0; i < arrayIndex; i++)
+            {
+                node = node.next;
+            }
+
+            for (int i = 0; i < _size - arrayIndex; i++)
+            {
+                array[i] = node.data;
+                node = node.next;
+            }
+        }
+
+        public object Clone()
+        {
+            T[] array = new T[Count];
+            CopyTo(array, 0);
+
+            return new SortedSLList<T>(_compareFunc, array);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new SLListEnumerator<T>(_head);
+        }
+
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
         }
     }
 }
+
