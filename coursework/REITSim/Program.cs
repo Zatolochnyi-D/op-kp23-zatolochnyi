ee8090c5cb7ad203dab14029e14e703b40cac0fa
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2023-06-04 13:34:12 +0300

    Major changes.
    Sorting rule for cities. Changed dependencies. Updated territory and property classes.

--- coursework/REITSim/Program.cs
+++ coursework/REITSim/Program.cs
@@ -9,13 +9,29 @@ namespace GameMechanics
         {
             Console.WriteLine("Start of testing:\n");
 
-            ClientGenerationTest();
+            //SortedSLListTest();
+
+            //Pause();
+
+            //CityLandSortingRuleTest();
+
+            //Pause();
+
+            //ClientGenerationTest();
+
+            //Pause();
+
+            //CityGenerationTest();
+
+            //Pause();
+
+            LandManagementTest();
 
             Pause();
 
-            CityGenerationTest();
+            //SharesTradingTest();
 
-            Pause();
+            //Pause();
         }
 
         static void Pause()
@@ -23,6 +39,97 @@ namespace GameMechanics
             Console.WriteLine("\nPress Enter to continue...");
             Console.ReadLine();
             Console.WriteLine();
+        }
+
+        static void SortedSLListTest()
+        {
+            Console.WriteLine("Sorted list test. Sort by descending array of integers: [1, 2, 7, 3, 4, 6, 2, 3, 8, 9, 4, 6, 3, 9, 2]");
+
+            SortedSLList<int> list = new((x, y) => x > y, new int[] { 1, 2, 7, 3, 4, 6, 2, 3, 8, 9, 4, 6, 3, 9, 2 });
+
+            Console.WriteLine("Expected output: 1, 2, 2, 2, 3, 3, 3, 4, 4, 6, 6, 7, 8, 9, 9");
+            Console.WriteLine("Got:");
+            foreach (int n in list)
+            {
+                Console.Write(n + ", ");
+            }
+            Console.WriteLine();
+        }
+
+        static void CityLandSortingRuleTest()
+        {
+            SortedSLList<Land> lands = new((x, y) =>
+            {
+                // compare belonging to the player
+                if (x.PlayerProperty && !y.PlayerProperty) return true;
+                if (!x.PlayerProperty && y.PlayerProperty) return false;
+
+                // compare presence of the building
+                if (!x.HaveBuilding && y.HaveBuilding) return true;
+                if (x.HaveBuilding && !y.HaveBuilding) return false;
+
+                // compare lands with buildings
+                if (x.HaveBuilding && y.HaveBuilding)
+                {
+                    // compare types of buildings
+                    if (x.Building.Requirement.CompareTo(y.Building.Requirement) == 1) return true;
+                    if (x.Building.Requirement.CompareTo(y.Building.Requirement) == -1) return false;
+
+                    // compare sizes of buildings
+                    if (x.Building.Requirement.Size > y.Building.Requirement.Size) return true;
+                    if (x.Building.Requirement.Size < y.Building.Requirement.Size) return false;
+                }
+                // compare lands without buildings
+                else
+                {
+                    // compare sizes of lands
+                    if (x.Size > y.Size) return true;
+                    if (x.Size < y.Size) return false;
+                }
+
+                return true;
+            });
+
+            City city = new(0);
+
+            Land[] landsArray = new Land[10];
+
+            landsArray[0] = new(city, 1);
+            landsArray[1] = new(city, 3);
+            landsArray[2] = new(city, 1);
+            landsArray[3] = new(city, 1);
+            landsArray[4] = new(city, 2);
+            landsArray[5] = new(city, 2);
+            landsArray[6] = new(city, 2);
+            landsArray[7] = new(city, 3);
+            landsArray[8] = new(city, 3);
+            landsArray[9] = new(city, 3);
+
+            landsArray[1].PlayerProperty = true;
+            landsArray[3].PlayerProperty = true;
+            landsArray[5].PlayerProperty = true;
+            landsArray[7].PlayerProperty = true;
+            landsArray[9].PlayerProperty = true;
+
+            landsArray[0].Build(1, "Factory");
+            landsArray[1].Build(2, "Factory");
+            landsArray[4].Build(1, "Shop");
+            landsArray[5].Build(1, "Shop");
+            landsArray[8].Build(2, "Factory");
+            landsArray[9].Build(1, "Factory");
+
+
+            foreach (Land land in landsArray)
+            {
+                lands.Add(land);
+            }
+
+            Console.WriteLine("Expected 0, 8, 4, 2, 6, 9, 1, 5, 3, 7.\n");
+            foreach (Land land in lands)
+            {
+                Console.Write(Array.IndexOf(landsArray, land) + "  ");
+            }
+            Console.WriteLine("\nSorting rule test completed. \n");
         }
 
         static void ClientGenerationTest()
@@ -46,7 +153,7 @@ namespace GameMechanics
         {
             Console.WriteLine($"Name: {client.Name}");
             Console.WriteLine($"Holds property?: {client.IsHolder}");
-            Console.WriteLine($"Requires: {client.Requirement.BuildingType.ToString()} of size {client.Requirement.Size}");
+            Console.WriteLine($"Requires: {client.Requirement.Type.ToString()} of size {client.Requirement.Size}");
             Console.WriteLine();
         }
 
@@ -73,51 +180,167 @@ namespace GameMechanics
             Console.WriteLine($"City taxation: {city.Taxation}");
             Console.WriteLine($"City lands:");
 
-            foreach (Land land in city.Lands)
+            foreach (Land land in city)
             {
-                Console.WriteLine($"    Land size: {land.Size}");
-                if (land.Building != null)
-                {
-                    Console.WriteLine($"    Land building:");
-                    Console.WriteLine($"        Building type: {land.Building.Requirement.BuildingType}");
-                    Console.WriteLine($"        Building size: {land.Building.Requirement.Size}");
-                    Console.WriteLine($"        Building income and maintenance: {land.Building.Income} {land.Building.Maintenance}");
-                }
-                else
-                {
-                    Console.WriteLine($"    Land building: -");
-                }
-                Console.WriteLine();
+                PrintLandInfo(land);
             }
         }
 
+        static void PrintLandInfo(Land land)
+        {
+            Console.WriteLine($"    Land size: {land.Size}");
+            Console.WriteLine($"    Land cost: {land.LandCost}");
+            Console.WriteLine($"    Land taxation: {land.LandTax}");
+            if (land.HaveBuilding)
+            {
+                Console.WriteLine($"    Land building:");
+                Console.WriteLine($"        Building type: {land.Building.Requirement.Type}");
+                Console.WriteLine($"        Building size: {land.Building.Requirement.Size}");
+                Console.WriteLine($"        Building profit and maintenance: {land.Building.Profit} {land.Building.Maintenance}");
+                Console.WriteLine($"        Building build and raze cost: {land.Building.BuilCost} {land.Building.RazeCost}");
+            }
+            else
+            {
+                Console.WriteLine($"    Land building: -");
+            }
+            Console.WriteLine();
+        }
 
-        static void PlayerStats(Player player)
+        static void LandManagementTest()
+        {
+            Console.WriteLine("Creating actors: city (tax = 10%), land in city (size = 1), client (requires factory of size 1), player");
+
+            City city = new(10);
+            city.AddLand(1);
+            Land refLand = city[0];
+
+            Client client = new(1, "Factory");
+
+            Player player = new("Player");
+
+            PrintCityInfo(city);
+            PrintClientInfo(client);
+            PrintPlayerInfo(player);
+
+            Console.WriteLine("Buy land. -50.0, income -5.0");
+
+            player.BuyLand(refLand);
+
+            //Console.WriteLine("Build factory of size 1. -75.0, income -15.0 (-20.0)");
+
+            //player.BuildBuilding(refLand, new Factory(refLand, 1));
+            //city.Lands[0].Building.CityTax = 10.0;
+
+            //Console.WriteLine("Make turn. Player money: 1000.0 - 125.0 - 20.0 = 855.0");
+
+            //player.NextTurn();
+
+            //PrintPlayerInfo(player);
+
+            //Console.WriteLine("Assign client to the factory. income +27.0 (+7.0)");
+
+            //player.RentOutBuilding(city.Lands[0], client);
+
+            //Console.WriteLine("Make 5 turns. Player money: 855.0 + 7.0 * 5 = 890.0");
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    player.NextTurn();
+            //}
+
+            //PrintPlayerInfo(player);
+
+            //Console.WriteLine("Raze factory");
+
+            //player.RazeBuilding(city.Lands[0]);
+
+            //PrintPlayerInfo(player);
+
+            //Console.WriteLine("Nothing changed. Factory is occupied. Disable autoextention and wait 15 turns:");
+            //Console.WriteLine("Player money: 890 + 7.0 * 15 = 995.0");
+
+            //city.Lands[0].Building.AutoExtention = false;
+
+            //for (int i = 0; i < 15; i++)
+            //{
+            //    player.NextTurn();
+            //}
+
+            //Console.WriteLine("Now factory is free:");
+
+            //PrintClientInfo(client);
+
+            //Console.WriteLine("Raze it! -15.0. Income = -5.0, Player money: 995.0 - 15.0 = 980.0");
+
+            //player.RazeBuilding(city.Lands[0]);
+
+            //PrintPlayerInfo(player);
+
+            //Console.WriteLine("Land management complete successfully.\n");
+        }
+
+        static void PrintPlayerInfo(Player player)
         {
             Console.WriteLine("Player stats:");
-            Console.WriteLine($"Player shares: {player.PlayerShares.Percent}");
-            Console.WriteLine($"Shares on exchange: {player.SharesOnExchange}");
-            Console.WriteLine($"Player reputation: {player.Reputation}");
-            Console.WriteLine($"Player money: {player.Money}");
-            Console.WriteLine($"Shares price: {player.SharePrice}");
-            Console.WriteLine($"Investor shares: {player.InvestorShares}");
+            Console.WriteLine($"    Player name: {player.Name}");
+            Console.WriteLine($"    Player money: {player.Money}");
+            Console.WriteLine($"    Player income: {player.Income}");
+            Console.WriteLine($"    Player reputation: {player.Reputation}");
+            Console.WriteLine($"    Player shares: {player.Shares}");
+            Console.WriteLine($"    Shares price: {player.SharePrice}");
+            Console.WriteLine($"    Shares on exchange: {player.SharesOnExchange}");
             Console.WriteLine();
         }
 
-        static void SharesTradingTest(double percent)
-        {
-            Player player = new("Name");
+        //static void SharesTradingTest()
+        //{
+        //    Console.WriteLine("Creating actors: player, city (tax = 0%), 5 clients (factories, size 2), factories");
 
-            PlayerStats(player);
+        //    Player player = new("player");
 
-            Console.WriteLine($"Sell {percent}% of shares:");
-            player.SharesToSell(percent);
+        //    Client[] clients = new Client[] { new(2, typeof(Factory)), new(2, typeof(Factory)), new(2, typeof(Factory)), new(2, typeof(Factory)), new(2, typeof(Factory)) };
 
-            PlayerStats(player);
+        //    City city = new(0);
 
-            Console.WriteLine();
+        //    for (int i = 0; i < 5; i++)
+        //    {
+        //        city.Lands.Add(new(0.0, 2));
+        //        city.Lands[i].Build(new Factory(2));
+        //        city.Lands[i].Building.CityTax = 0.0;
+        //        player.BuyLand(city.Lands[i]);
+        //        player.RentOutBuilding(city.Lands[i], clients[i]);
+        //    }
 
-            //TODO: make test when player will have income
-        }
+        //    PlayerStats(player);
+
+        //    Console.WriteLine("Sell 50% of shares:");
+
+        //    player.SharesToSell(50.0);
+
+        //    PlayerStats(player);
+
+        //    Console.WriteLine("Wait 10 turns:");
+
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        player.NextTurn();
+        //    }
+
+        //    PlayerStats(player);
+
+
+        //    //Player player = new("Name");
+
+        //    //PlayerStats(player);
+
+        //    //Console.WriteLine($"Sell {percent}% of shares:");
+        //    //player.SharesToSell(percent);
+
+        //    //PlayerStats(player);
+
+        //    //Console.WriteLine();
+
+        //    //TODO: make test when player will have income
+        //}
     }
 }
--- coursework/REITSim/Property.cs
+++ coursework/REITSim/Property.cs
@@ -8,8 +8,8 @@ namespace GameMechanics
         // === building stats ===
         protected Requirement _requirement;
         protected double _maintenance;
-        protected double _cityTax;
-        protected double _income;
+        protected double _profit;
+        protected Land _parentLand;
 
         // === rent info ===
         protected int _oneRentTime;
@@ -17,31 +17,38 @@ namespace GameMechanics
         protected bool _autoExtension;
         protected Client? _holder;
 
-        public virtual double CityTax { get { return _cityTax; } set { _cityTax = value; } }
-        public virtual double TotalCost => (_income * (1.0 - _cityTax / 100.0)) * (Occupied ? 1.0 : 0.0) - _maintenance;
-        public virtual bool Occupied => _holder != null;
-        public Requirement Requirement => _requirement;
-        public double Maintenance => _maintenance;
-        public double Income => _income;
+        public abstract double BuilCost { get; }
+        public abstract double RazeCost { get; }
 
-        public Building(int size)
+        public virtual Requirement Requirement => _requirement;
+        public virtual double Maintenance => _maintenance;
+        public virtual double Profit => _profit;
+        public virtual Land ParentLand => _parentLand;
+        
+        public virtual bool Occupied => _holder != null;
+        public virtual bool AutoExtention { get { return _autoExtension; } set { _autoExtension = value; } }
+
+        public virtual double Income => (_profit * (1.0 - _parentLand.ParentCity.Taxation / 100.0)) * (Occupied ? 1.0 : 0.0) - _maintenance;
+
+        public Building(Land land)
         {
             _rentExpireAfter = 0;
             _autoExtension = true;
             _holder = null;
-
-            _requirement = new(size, this.GetType());
-            _cityTax = 0.0;
+            _parentLand = land;
         }
 
         public virtual void RentOut(Client client)
         {
             _holder = client;
+            _holder.Rent();
+
             _rentExpireAfter = _oneRentTime;
         }
 
         public virtual void Release()
         {
+            _holder.Leave();
             _holder = null;
         }
 
@@ -69,71 +76,105 @@ namespace GameMechanics
 
     public class Factory : Building
     {
-        public const double Maintenance = 15.0;
-        public const double Profit = 30.0;
-        public const int RentTime = 20;
+        public const double BaseMaintenance = 15.0;
+        public const double BaseProfit = 30.0;
+        public const int BaseRentTime = 20;
 
-        public Factory(int size) : base(size)
+        public override double BuilCost => Math.Round(BaseMaintenance * _requirement.Size * 2.0, 2);
+
+        public override double RazeCost => Math.Round(BaseMaintenance * _requirement.Size * 0.5, 2);
+
+        public Factory(Land land, int size) : base(land)
         {
-            _maintenance = Maintenance * size;
-            _income = Profit * size;
-            _oneRentTime = RentTime;
+            _requirement = new(size, "Factory");
+
+            _maintenance = BaseMaintenance * _requirement.Size;
+            _profit = BaseProfit * _requirement.Size;
+            _oneRentTime = BaseRentTime;
         }
     }
+
 
     public class Shop : Building
     {
-        public const double Maintenance = 5.0;
-        public const double Profit = 12.0;
-        public const int RentTime = 20;
+        public const double BaseMaintenance = 5.0;
+        public const double BaseProfit = 12.0;
+        public const int BaseRentTime = 20;
 
-        public Shop(int size) : base(size)
+        public override double BuilCost => Math.Round(BaseMaintenance * _requirement.Size, 2);
+
+        public override double RazeCost => Math.Round(BaseMaintenance * _requirement.Size, 2);
+
+        public Shop(Land land, int size) : base(land)
         {
-            _maintenance = Maintenance * size;
-            _income = Profit * size;
-            _oneRentTime = RentTime;
+            _requirement = new(size, "Shop");
+
+            _maintenance = BaseMaintenance * _requirement.Size;
+            _profit = BaseProfit * _requirement.Size;
+            _oneRentTime = BaseRentTime;
         }
     }
+
 
     public class Warehouse : Building
     {
-        public const double Maintenance = 5.0;
-        public const double Profit = 15.0;
-        public const int RentTime = 30;
+        public const double BaseMaintenance = 5.0;
+        public const double BaseProfit = 15.0;
+        public const int BaseRentTime = 30;
 
-        public Warehouse(int size) : base(size)
+        public override double BuilCost => Math.Round(BaseMaintenance * _requirement.Size, 2);
+
+        public override double RazeCost => Math.Round(BaseMaintenance * _requirement.Size, 2);
+
+        public Warehouse(Land land, int size) : base(land)
         {
-            _maintenance = Maintenance * size;
-            _income = Profit * size;
-            _oneRentTime = RentTime;
+            _requirement = new(size, "Warehouse");
+
+            _maintenance = BaseMaintenance * _requirement.Size;
+            _profit = BaseProfit * _requirement.Size;
+            _oneRentTime = BaseRentTime;
         }
     }
+
 
     public class Office : Building
     {
-        public const double Maintenance = 5.0;
-        public const double Profit = 15.0;
-        public const int RentTime = 30;
+        public const double BaseMaintenance = 5.0;
+        public const double BaseProfit = 15.0;
+        public const int BaseRentTime = 30;
 
-        public Office(int size) : base(size)
+        public override double BuilCost => Math.Round(BaseMaintenance * _requirement.Size, 2);
+
+        public override double RazeCost => Math.Round(BaseMaintenance * _requirement.Size, 2);
+
+        public Office(Land land, int size) : base(land)
         {
-            _maintenance = Maintenance * size;
-            _income = Profit * size;
-            _oneRentTime = RentTime;
+            _requirement = new(size, "Office");
+
+            _maintenance = BaseMaintenance * _requirement.Size;
+            _profit = BaseProfit * _requirement.Size;
+            _oneRentTime = BaseRentTime;
         }
     }
 
+
     public class ShoppingCentre : Building
     {
-        public const double Maintenance = 15.0;
-        public const double Profit = 30.0;
-        public const int RentTime = 30;
+        public const double BaseMaintenance = 15.0;
+        public const double BaseProfit = 30.0;
+        public const int BaseRentTime = 30;
 
-        public ShoppingCentre(int size) : base(size)
+        public override double BuilCost => Math.Round(BaseMaintenance * _requirement.Size, 2);
+
+        public override double RazeCost => Math.Round(BaseMaintenance * _requirement.Size, 2);
+
+        public ShoppingCentre(Land land, int size) : base(land)
         {
-            _maintenance = Maintenance * size;
-            _income = Profit * size;
-            _oneRentTime = RentTime;
+            _requirement = new(size, "ShoppingCentre");
+
+            _maintenance = BaseMaintenance * _requirement.Size;
+            _profit = BaseProfit * _requirement.Size;
+            _oneRentTime = BaseRentTime;
         }
     }
 }
